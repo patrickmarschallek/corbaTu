a1de202b4c0eda547dc9d5c06fbdac59d97f2edf
Tim Strehlow <tim.strehlow@googlemail.com>
2013-05-19 10:15:24 +0200

    implementation a) + b)

--- CorbaClient/src/CorbaClient.java
+++ CorbaClient/src/CorbaClient.java
@@ -1,11 +1,16 @@
+
 import org.omg.CORBA.ORB;
+import org.omg.CORBA.Request;
+import org.omg.CORBA.WrongTransaction;
 import org.omg.CosNaming.NameComponent;
 import org.omg.CosNaming.NamingContext;
 import org.omg.CosNaming.NamingContextHelper;
 
+import Stock.InvalidStockException;
 import Stock.Quoter;
 import Stock.QuoterHelper;
 import Stock.StockQuote;
+import Stock.StockQuoteHolder;
 
 public class CorbaClient {
 	public static void main(String args[]) {
@@ -24,18 +29,83 @@ public class CorbaClient {
 			NameComponent path[] = { nc };
 			Quoter quoterRef = QuoterHelper.narrow(ncRef.resolve(path));
 
-			StockQuote stockQuote;
-			try {
-				int stockId = Integer.parseInt(args[0]);
-				stockQuote = quoterRef.getQuoteById(stockId);
-			} catch (NumberFormatException e) {
-				stockQuote = quoterRef.getQuoteByName(args[0]);
+			if (args[1].equals("a")) {
+				useStaticInvocation(quoterRef, args);
+			} else if (args[1].equals("b")) {
+				useDynamicInvocation(quoterRef, args);
+			} else if (args[1].equals("c")) {
+				useOnewayCalls(quoterRef, args);
+			} else if (args[1].equals("d")) {
+				useAMI(quoterRef, args);
+
 			}
-			System.out.println(stockQuote);
 
 		} catch (Exception e) {
 			System.out.println("ERROR : " + e);
 			e.printStackTrace(System.out);
 		}
+	}
+
+	private static void useStaticInvocation(Quoter quoterRef, String args[])
+			throws InvalidStockException {
+		StockQuote stockQuote;
+		try {
+			int stockId = Integer.parseInt(args[0]);
+			stockQuote = quoterRef.getQuoteById(stockId);
+		} catch (NumberFormatException e) {
+			stockQuote = quoterRef.getQuoteByName(args[0]);
+		}
+		printQuote(stockQuote);
+	}
+
+	private static void useDynamicInvocation(Quoter quoterRef, String args[]) {
+
+		// Create a Request
+		Request r = quoterRef._request("getQuoteById");
+
+		// Add the in string
+		// r.add_in_arg().insert_string(args[0]);
+		int id = Integer.parseInt(args[0]);
+		r.add_in_arg().insert_long(id);
+
+		// Add the Streamable for return value
+		StockQuoteHolder quoteHolder = new StockQuoteHolder();
+		r.return_value().insert_Streamable(quoteHolder);
+
+		// Invoke the Request
+		r.send_deferred();
+
+
+		try {
+			while(!r.poll_response()){
+				Thread.sleep(1000);
+			}
+			r.get_response();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (WrongTransaction e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		StockQuote quote = quoteHolder.value;
+
+		printQuote(quote);
+	}
+
+	private static void useOnewayCalls(Quoter quoterRef, String args[]) {
+
+	}
+
+	private static void useAMI(Quoter quoterRef, String args[]) {
+
+	}
+
+	private static void printQuote(StockQuote quote) {
+		System.out.println("StockQuote");
+		System.out.println("\tID: " + quote.id);
+		System.out.println("\tName: " + quote.name);
+		System.out.println("\tPrice: " + quote.price);
 	}
 }
--- CorbaServer/src/CorbaServer.java
+++ CorbaServer/src/CorbaServer.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import org.omg.CORBA.ORB;
 import org.omg.CosNaming.NameComponent;
 import org.omg.CosNaming.NamingContextExt;
@@ -24,6 +27,7 @@ public class CorbaServer {
 
 			// create servant and register it with the ORB
 			QuoterServant quoterImpl = new QuoterServant();
+			quoterImpl.generateTestData();
 			quoterImpl.setORB(orb);
 
 			// get object reference from the servant
@@ -61,6 +65,8 @@ public class CorbaServer {
 class QuoterServant extends QuoterPOA {
 	private ORB orb;
 
+	private List<StockQuote> quotes;
+
 	public void setORB(ORB orb_val) {
 		orb = orb_val;
 	}
@@ -68,19 +74,41 @@ class QuoterServant extends QuoterPOA {
 	@Override
 	public StockQuote getQuoteByName(String stockName)
 			throws InvalidStockException {
-		// TODO Auto-generated method stub
+		for (StockQuote q : quotes) {
+			if (q.name.equals(stockName)) {
+				return q;
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public StockQuote getQuoteById(int stockId) throws InvalidStockException {
-		// TODO Auto-generated method stub
+		System.out.println("getQuoteById " + stockId);
+		for (StockQuote q : quotes) {
+			if (q.id == stockId) {
+				System.out.println("returning quote");
+				return q;
+			}
+		}
 		return null;
 	}
 	
 		  // implement shutdown() method
 	public void shutdown() {
 		orb.shutdown(false);
+	}
+
+	public void generateTestData() {
+		StockQuote quote = new StockQuote(0, "test0", 1.2);
+		StockQuote quote1 = new StockQuote(1, "test1", 3.4);
+		StockQuote quote2 = new StockQuote(2, "test2", 5.6);
+
+		quotes = new ArrayList<>();
+
+		quotes.add(quote);
+		quotes.add(quote1);
+		quotes.add(quote2);
 	}
 
 }
