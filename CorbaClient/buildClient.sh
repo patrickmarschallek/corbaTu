947df06ab2f2ae4eb5c58ab4f81c23d98e507c3f
Tim Strehlow <tim.strehlow@googlemail.com>
2013-05-22 12:42:12 +0200

    spaces in paths

--- CorbaClient/buildClient.sh
+++ CorbaClient/buildClient.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DIR=$(pwd)
-mkdir -p $DIR/bin/
-javac -cp ../lib/*:$DIR/bin -d $DIR/bin -sourcepath src $DIR/src/CorbaClient.java
-javac -cp ../lib/*:$DIR/bin -d $DIR/bin -sourcepath test $DIR/test/CorbaClientTest.java
+mkdir -p "$DIR/bin/"
+javac -cp "../lib/*:$DIR/bin" -d "$DIR/bin" -sourcepath src "$DIR/src/CorbaClient.java"
+javac -cp "../lib/*:$DIR/bin" -d "$DIR/bin" -sourcepath test "$DIR/test/CorbaClientTest.java"
--- CorbaServer/buildServer.sh
+++ CorbaServer/buildServer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 DIR=$(pwd)
-mkdir -p $DIR/bin/
-javac -cp ../lib/*:$DIR/bin -d $DIR/bin -sourcepath src $DIR/src/CorbaServer.java
+mkdir -p "$DIR/bin/"
+javac -cp "../lib/*:$DIR/bin" -d "$DIR/bin" -sourcepath src "$DIR/src/CorbaServer.java"
