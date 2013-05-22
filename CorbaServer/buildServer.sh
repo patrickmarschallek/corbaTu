b98e205e4bd4b53de01707696239d7ec7fdc24af
Patte <patrick.marschallek@gmail.com>
2013-05-22 11:07:26 +0200

    fixed classpathes in the script

--- CorbaServer/buildServer.sh
+++ CorbaServer/buildServer.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-BASEDIR=$(dirname $0)
-echo $BASEDIR
 DIR=$(pwd)
-echo $DIR
 mkdir -p $DIR/bin/
-javac -cp $JACORB_HOME/../lib/jacorb.jar:$JACORB_HOME/../lib/slf4j-api-1.6.4.jar:$JACORB_HOME/../lib/slf4j-jdk14-1.6.4.jar -d $DIR/bin -sourcepath src $DIR/src/CorbaServer.java
+javac -cp ../lib/*:$DIR/bin -d $DIR/bin -sourcepath src $DIR/src/CorbaServer.java
